d7834096dded10d98f762dda9469991cfd1ffccd
mehmetunlu <munlu4037@gmail.com>
2023-04-20 00:56:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(action="roller-skate"){
+    return `This Saturday, I want to ${action}!`}
+
+function mondayWork(activity="go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(sign="*"){
+    const innerfunction = function(adjective="special"){
+        return `You are ${sign}${adjective}${sign}!`
+    }
+    return innerfunction
+}
+
